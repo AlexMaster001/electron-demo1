3d626e16a6b4f1647911607e9a757a8052cfd2fd
sseezov <sseezov@mail.ru>
2024-09-12 19:18:58 +0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const createWindow = () => {
   })
 
   win.loadFile('index.html')
+  win.webContents.openDevTools()
 }
+
 
 app.whenReady().then(() => {
   createWindow()
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const table = document.querySelector('.table');
+
+table.addEventListener('click', () => {
+  console.log(121);
+})
+
