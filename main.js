2170d387c0349932c394e98b8553c82c4d8bdc13
sseezov <sseezov@mail.ru>
2024-09-15 10:33:23 +0700

    add cross-platform features with comments

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ const createWindow = () => {
   })
 
   win.loadFile('index.html')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools() - открывает инструменты разработчика в приложении
 }
 
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow() // эта строка открывает окно на Маке, если у нашего приложения нет открытых окон, но мы его вызвали
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit() // эта строка выключает приложение на винде и линуксе, если закрыты все его окна
 })
