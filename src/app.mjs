726d65636f6770a5e2af0b1b567475a6171e90d4
sseezov <sseezov@mail.ru>
2024-09-15 10:14:27 +0700

    add readme, rename main.js

---
+++ src/app.mjs
@@ -0,0 +1,15 @@
+import users from './db.mjs';
+
+const createRow = (userData) => {
+  const table = document.querySelector('.table');
+  const tr = document.createElement('tr');
+  const { name, email, age, phone } = userData;
+  [name, email, age, phone].forEach((data) => {
+    const td = document.createElement('td');
+    td.textContent = data;
+    tr.appendChild(td);
+  })
+  table.appendChild(tr)
+}
+
+users.forEach((user) => createRow(user));
