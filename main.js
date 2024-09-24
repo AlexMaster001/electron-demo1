18c7a85fa8df551b7db51a369ac682519c972547
sseezov <sseezov@mail.ru>
2024-09-24 19:55:25 +0700

    preload fix

--- main.js
+++ main.js
@@ -29,6 +29,9 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 500,
       height: 500,
+      webPreferences: {
+        preload: path.join(__dirname, 'src/preload.js')
+      }
     });
 
     win.loadFile('user.html')
